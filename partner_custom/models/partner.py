c487c9e7e89055cb3e07db295a637d175fc8d518
Omar <omarcs7r@gmail.com>
2017-02-22 20:06:36 +0100

    [IMP] partner_custom: Se añade el campo de descuento global a modo informativo

--- partner_custom/models/partner.py
+++ partner_custom/models/partner.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
+import odoo.addons.decimal_precision as dp
 
 
 class ResPartner(models.Model):
@@ -12,3 +13,5 @@ class ResPartner(models.Model):
     harbor_ids = fields.Many2many("res.harbor", string="Harbors")
     propietary = fields.\
         Char("Propietary", default=lambda self: self.env.user.company_id.ref)
+    partner_discount = fields.Float("Partner discount", default=0.0,
+                                    digits=dp.get_precision('Discount'))
