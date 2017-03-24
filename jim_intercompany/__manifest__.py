27ae7dbf381b98df1f51be2bda45ab5f226843cb
Roodin <santi@comunitea.com>
2017-03-24 15:37:26 +0100

    [ADD]. jim_intercompany. Añadido modulo

---
+++ jim_intercompany/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Jim Intercompany',
+    'version': '10.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'purchase',
+        'sale_stock',
+    ],
+    'contributors': [
+        "Comunitea ",
+    ],
+    "data": [
+    ],
+    "installable": True
+}
