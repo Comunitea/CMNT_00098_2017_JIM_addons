f2c9aa0ae5f9f0b667ee29decda8bffaa0d4881c
javierjcf <javierjcf@gmail.com>
2017-03-29 17:28:08 +0200

    [ADD]jim_stock: Stocks_globales

---
+++ jim_stock/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Jim Stock',
+    'version': '10.0.0.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'stock',
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Javier Colmenero <javier@comunitea.com>"
+    ],
+    "data": [
+        'views/product_view.xml',
+    ],
+    "installable": True
+}
