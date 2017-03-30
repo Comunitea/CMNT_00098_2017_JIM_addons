da695e76eb1d10c07b39c313c26eb3bdbd94a28b
javierjcf <javierjcf@gmail.com>
2017-03-30 17:21:43 +0200

    [ADD]jim_telesale: Integración con campo sotock global

--- jim_telesale/models/sale.py
+++ jim_telesale/models/sale.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, api, _
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
@@ -13,3 +13,18 @@ class SaleOrder(models.Model):
         OWERWRITED in order to pass the state to pending or lqdr
         """
         self.browse(order_id).action_lqdr_pending()
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def ts_product_id_change(self, product_id, partner_id):
+        res = super(SaleOrderLine, self).ts_product_id_change(product_id,
+                                                              partner_id)
+
+        product = self.env['product.product'].browse(product_id)
+        res.update({
+            'global_available_stock': product.global_available_stock
+        })
+        return res
--- jim_telesale/static/src/js/new_order_widgets.js
+++ jim_telesale/static/src/js/new_order_widgets.js
@@ -1,6 +1,5 @@
 odoo.define('jim_telesale.new_order_widgets2', function (require) {
 "use strict";
-
 var Model = require('web.DataModel');
 var NewOrderWidgets = require('telesale.new_order_widgets');
 
@@ -30,6 +29,49 @@ var ProductInfoOrderWidget = NewOrderWidgets.ProductInfoOrderWidget.include({
             }   
         }
     },
-    });
 });
+
+var OrderlineWidget = NewOrderWidgets.OrderlineWidget.include({
+    // Full overwrited to get global_available_stock
+    call_product_id_change: function(product_id){
+        var self = this;
+        $.when( self.update_stock_product(product_id) ).done(function(){
+            var customer_id = self.ts_model.db.partner_name_id[self.order.get('partner')];
+            var model = new Model("sale.order.line");
+            model.call("ts_product_id_change", [product_id, customer_id])
+            .then(function(result){
+                var product_obj = self.ts_model.db.get_product_by_id(product_id);
+                var uom_obj = self.ts_model.db.get_unit_by_id(product_obj.uom_id[0])
+            
+                self.model.set('code', product_obj.default_code || "");
+                self.model.set('product', product_obj.display_name || "");
+                self.model.set('taxes_ids', result.tax_id || []); //TODO poner impuestos de producto o vacio
+                self.model.set('unit', self.model.ts_model.db.unit_by_id[result.product_uom].name);
+                self.model.set('qty', result.product_uom_qty);
+                self.model.set('discount', 0.0);
+                self.model.set('pvp', self.ts_model.my_round( result.price_unit));
+                self.model.set('global_available_stock', self.ts_model.my_round( result.global_available_stock));
+               
+                var subtotal = self.model.get('pvp') * self.model.get('qty') * (1 - self.model.get('discount') / 100.0)
+                self.model.set('total', self.ts_model.my_round(subtotal || 0,2));
+                self.refresh('qty');
+                self.$('.col-qty').select()
+            });
+        })
+        .fail(function(){
+            // alert(_t("NOT WORKING"));
+        })
+    },
+    renderElement: function(){
+        //Because i dont know how to add to Backbone orderline defaults.
+        // this.model.set('global_available_stock', 0.0)
+        this._super();
+        var self = this;
+        var product_name = self.model.get('product')
+    },
+
+});
+
+});
+
 
