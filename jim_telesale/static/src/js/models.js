4efff72e7d6f2b064ec2a1b7a38bbd6e20b3c1a1
javierjcf <javierjcf@gmail.com>
2017-04-18 19:30:37 +0200

    [ADD]jim_telesale: Campos LQDR y Almacén en las líneas

--- jim_telesale/static/src/js/models.js
+++ jim_telesale/static/src/js/models.js
@@ -1,18 +1,8 @@
-odoo.define('jim_telesale.models', function (require) {
+odoo.define('jim_telesale.models2', function (require) {
 "use strict";
 var TsModels = require('telesale.models');
 
-// var OrderlineExt = TsModels.Orderline.extend({
-//     defaults: _.extend({},TsModels.Orderline.prototype.defaults, {global_available_stock: 50})
-// });
-
-// return {Orderline: OrderlineExt}
-
-
-// var OrderlineExt = TsModels.Orderline.extend({
-//     defaults: {global_available_stock: 50}
-// });
-// _.extend(OrderlineExt.prototype.defaults, TsModels.Orderline.prototype.defaults);
-
-
-// return {Orderline: OrderlineExt}
+// Overwrite backbone TsModel function to get extra fields lqdr and route_name;
+TsModels.TsModel.prototype._get_product_fields = function(){
+    return  ['display_name', 'default_code', 'list_price', 'standard_price', 'uom_id', 'taxes_id', 'weight', 'lqdr', 'route_name']}
+});
--- jim_telesale/static/src/js/new_order_widgets.js
+++ jim_telesale/static/src/js/new_order_widgets.js
@@ -2,34 +2,37 @@ odoo.define('jim_telesale.new_order_widgets2', function (require) {
 "use strict";
 var Model = require('web.DataModel');
 var NewOrderWidgets = require('telesale.new_order_widgets');
+var core = require('web.core');
+var _t = core._t;
 
-var ProductInfoOrderWidget = NewOrderWidgets.ProductInfoOrderWidget.include({
-    set_default_values: function(){
-        this._super();
-        this.route = ""; 
-        this.lqdr = "";
-    },
-    change_product: function(){
-        this._super();
-        var self = this
-        var line_product = this.selected_line.get("product")
-        self.n_line = self.selected_line.get('n_line') + " / " + self.ts_model.get('selectedOrder').get('orderLines').length;
-        if (line_product != ""){
-            var product_id = this.ts_model.db.product_name_id[line_product]
-            var partner_name = this.ts_model.get('selectedOrder').get('partner');
-            var partner_id = this.ts_model.db.partner_name_id[partner_name];
-            if (product_id && partner_id){
-                var model = new Model('product.product');
-                model.call("get_product_info",[product_id,partner_id])
-                    .then(function(result){
-                        self.route = result.route;
-                        self.lqdr = result.lqdr;
-                        self.renderElement();
-                    });
-            }   
-        }
-    },
-});
+// Ejemplo de como añadir al widget de producto nuevos campos
+// var ProductInfoOrderWidget = NewOrderWidgets.ProductInfoOrderWidget.include({
+//     set_default_values: function(){
+//         this._super();
+//         this.route = ""; 
+//         this.lqdr = "";
+//     },
+//     change_product: function(){
+//         this._super();
+//         var self = this
+//         var line_product = this.selected_line.get("product")
+//         self.n_line = self.selected_line.get('n_line') + " / " + self.ts_model.get('selectedOrder').get('orderLines').length;
+//         if (line_product != ""){
+//             var product_id = this.ts_model.db.product_name_id[line_product]
+//             var partner_name = this.ts_model.get('selectedOrder').get('partner');
+//             var partner_id = this.ts_model.db.partner_name_id[partner_name];
+//             if (product_id && partner_id){
+//                 var model = new Model('product.product');
+//                 model.call("get_product_info",[product_id,partner_id])
+//                     .then(function(result){
+//                         self.route = result.route;
+//                         self.lqdr = result.lqdr;
+//                         self.renderElement();
+//                     });
+//             }   
+//         }
+//     },
+// });
 
 var OrderlineWidget = NewOrderWidgets.OrderlineWidget.include({
     refresh: function(focus_key){
@@ -60,6 +63,20 @@ var OrderlineWidget = NewOrderWidgets.OrderlineWidget.include({
         self.renderElement();
         self.$('.col-'+ focus_key).focus()
     },
+    renderElement: function() {
+        var prod_name = this.$('.col-product').val();
+        var product_id = this.ts_model.db.product_name_id[prod_name];
+        if (product_id){
+            var product_obj = this.ts_model.db.get_product_by_id(product_id);
+            var lqdr = (product_obj.lqdr)  ? _t("Yes") : _t("No")
+            this.model.set('lqdr', lqdr);
+            this.model.set('route_name', product_obj.route_name);
+        }
+        else{
+            this.model.set('global_available_stock', 0.0)
+        }
+        this._super();
+    }
 });
 
 });
