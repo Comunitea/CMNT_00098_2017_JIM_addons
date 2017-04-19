2ba845e0fb482689a03cd2d137b25c49da77ad3a
javierjcf <javierjcf@gmail.com>
2017-04-19 12:25:48 +0200

    [IMP]jim_telesale: Mejora al heredar backbone

--- jim_telesale/static/src/js/models.js
+++ jim_telesale/static/src/js/models.js
@@ -1,8 +1,15 @@
 odoo.define('jim_telesale.models2', function (require) {
 "use strict";
 var TsModels = require('telesale.models');
+var TsModelSuper = TsModels.TsModel
 
-// Overwrite backbone TsModel function to get extra fields lqdr and route_name;
-TsModels.TsModel.prototype._get_product_fields = function(){
-    return  ['display_name', 'default_code', 'list_price', 'standard_price', 'uom_id', 'taxes_id', 'weight', 'lqdr', 'route_name']}
+TsModels.TsModel = TsModels.TsModel.extend({
+    _get_product_fields: function(){
+        var res = TsModelSuper.prototype._get_product_fields.call(this,{});
+        res.push('lqdr', 'route_name')
+        return res
+    },
+});
+
+
 });
