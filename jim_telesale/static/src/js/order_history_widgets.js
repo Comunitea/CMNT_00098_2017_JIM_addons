7ca90c3b17a67dc86b723816a436f4db6e53fc0b
javierjcf <javierjcf@gmail.com>
2017-04-22 19:53:43 +0200

    [REF]jim_telesale: Movida herenciade widget para chained_discount

--- jim_telesale/static/src/js/order_history_widgets.js
+++ jim_telesale/static/src/js/order_history_widgets.js
@@ -1,9 +1,9 @@
 odoo.define('jim_telesale.order_history_widgets2', function (require) {
 "use strict";
 
-var OrderHistory = require('telesale.OrderHistory');
+var NewOrderWidgets = require('telesale.new_order_widgets');
 
-var HistorylineWidget = OrderHistory.HistorylineWidget.include({
+var OrderWidget = NewOrderWidgets.OrderWidget.include({
     get_line_fields: function(){
         var res = this._super();
         res.push('chained_discount')
