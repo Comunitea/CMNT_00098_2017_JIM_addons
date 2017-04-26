e05b5aa54fe136dba619c08a0614e9b1a803886a
jesusVMayor <jesus@comunitea.com>
2017-04-26 18:27:33 +0200

    [IMP]custom_sale_order_variant_mgmt: Añadido módulo para gestionar variantes en pedidos de venta.

---
+++ custom_sale_order_variant_mgmt/models/sale_order.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class SaleOrderLineTemplate(models.Model):
+
+    _name = 'sale.order.line.template'
+    _inherit = 'sale.order.line'
+
+    product_template = fields.Many2one(
+        'product.template', string='Product',
+        domain=[('sale_ok', '=', True), ('product_variant_count', '=', 1)],
+        change_default=True, ondelete='restrict', required=True)
+
+    order_lines = fields.One2many('sale.order.line', 'template_line')
+    lines_qty = fields.Integer(compute='_compute_order_lines_qty')
+    price_subtotal = fields.Monetary(
+        compute='_compute_amount', string='Subtotal', readonly=True, store=True)
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        for line in self:
+            line.price_subtotal = sum([x.price_subtotal for x in line.order_lines])
+
+    @api.multi
+    def unlink(self):
+        for tempalte in self:
+            tempalte.order_lines.unlink()
+        return super(SaleOrderLineTemplate,self).unlink()
+
+    @api.multi
+    def write(self, vals):
+        for template in self:
+            line_vals = vals
+            if self.lines_qty > 1:
+                line_vals.pop('product_id', False)
+                line_vals.pop('product_uom_qty', False)
+                line_vals.pop('price_unit', False)
+                line_vals.pop('purchase_price', False)
+                line_vals.pop('name', False)
+            self.order_lines.write(vals)
+        return super(SaleOrderLineTemplate, self).write(vals)
+
+    @api.model
+    def create(self, vals):
+        if not self._context.get('no_create_line', False):
+            new_line = self.env['sale.order.line'].create(vals)
+            vals['order_lines'] = [(6, 0, [new_line.id])]
+        return super(SaleOrderLineTemplate, self).create(vals)
+
+    def _compute_order_lines_qty(self):
+        for template in self:
+            template.lines_qty = len(template.order_lines)
+
+    @api.onchange('product_template')
+    def onchange_template(self):
+        if not self.product_template:
+            return
+        self.product_id = self.product_template.product_variant_ids[0]
+
+
+class SaleOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    template_line = fields.Many2one('sale.order.line.template')
+
+    @api.model
+    def create(self, vals):
+        if self._context.get('template_line', False):
+            vals['template_line'] = self._context.get('template_line', False)
+        return super(SaleOrderLine, self).create(vals)
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    template_lines = fields.One2many('sale.order.line.template', 'order_id')
