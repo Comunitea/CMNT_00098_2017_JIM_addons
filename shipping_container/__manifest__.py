89cc3a15b03157fe968dcffe7b174fb859f9d94d
kiko <kiko@comunitea.com>
2017-06-19 12:06:40 +0200

    [IMP] Módulo para manejo de contenedores

---
+++ shipping_container/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Kiko Sanchez <kiko@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Shipping Container',
+    'version': '10.0.1.0.0',
+    'author': 'Comunitea ',
+    "category": "Custom",
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+        'stock',
+
+    ],
+    'contributors': [
+        "Comunitea ",
+        "Kiko Sanchez <kiko@comunitea.com>",
+
+    ],
+    "data": [
+        #'views/purchase_order.xml',
+        'views/stock_picking.xml',
+        'views/shipping_container.xml',
+        #'security/shipping_container.csv'
+
+    ],
+    "installable": True
+}
---
+++ shipping_container/models/stock_picking.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea - Kiko Sanchez <kiko@comunitea.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+class StockPicking(models.Model):
+
+    _inherit = 'stock.picking'
+
+    shipping_container_id = fields.Many2one("shipping.container", "Shipping container",
+                                            domain="[('state','in',['draft', 'loading'])]")
+
+    def write(self, vals):
+        res = super(StockPicking, self).write(vals)
+
+        shipping_container_id = vals.get('shipping_container_id', False)
+        if shipping_container_id:
+            shipping_container = self.env['shipping.container'].browse(shipping_container_id)
+            if shipping_container.state != 'loading':
+                raise ValidationError(_('Container not in "loading" state'))
+            self.min_date = shipping_container.date_expected
+        return res
+
+class StockMove(models.Model):
+
+    _inherit = 'stock.move'
+    shipping_container_id = fields.Many2one(related='picking_id.shipping_container_id')
