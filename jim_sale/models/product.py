9cbf9ae4c23e75060e4878a199797e60f03e2928
javierjcf <javierjcf@gmail.com>
2017-07-14 12:30:40 +0200

    [FIX]jim_sale: Menús y botón de acceso a precios de cliente

--- jim_sale/models/product.py
+++ jim_sale/models/product.py
@@ -1,25 +1,39 @@
 # -*- coding: utf-8 -*-
 # © 2016 Comunitea - Javier Colmenero <javier@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import fields, models
+from odoo import fields, models, api
 import odoo.addons.decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    @api.multi
+    def _get_customer_prices_count(self):
+        for tmpl in self:
+            tmpl.customer_prices_count = len(tmpl.customer_tmpl_prices)
+
     lqdr = fields.Boolean('LQDR')
     customer_tmpl_prices = fields.One2many('customer.price', 'product_tmpl_id',
                                            'Customer Prices')
+    customer_prices_count = fields.\
+      Integer(compute='_get_customer_prices_count', string='#Prices')
 
-
+    
 class ProductPorduct(models.Model):
     _inherit = "product.product"
 
+    @api.multi
+    def _get_customer_prices_count(self):
+        for prod in self:
+            prod.customer_prices_count = len(prod.customer_product_prices)
+
     customer_product_prices = fields.One2many('customer.price', 'product_id',
                                               'Customer Prices')
+    customer_prices_count = fields.\
+      Integer(compute='_get_customer_prices_count', string='#Prices')
 
-
+   
 class CustomerPrice(models.Model):
     _name = "customer.price"
 
