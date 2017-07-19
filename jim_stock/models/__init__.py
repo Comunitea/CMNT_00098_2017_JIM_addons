52068ae0acf2cb96d57b9c88b60896f631955686
javierjcf <javierjcf@gmail.com>
2017-07-19 11:49:21 +0200

    [FIX]jim_stock: product_name=False en inventory_lone. Bug odoo error seguridad

--- jim_stock/models/__init__.py
+++ jim_stock/models/__init__.py
@@ -4,4 +4,5 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 import product
 import stock
+import stock_inventory
 
---
+++ jim_stock/models/stock_inventory.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea Servicios Tecnologicos (<http://www.comunitea.com>)
+# Kiko Sanchez (<kiko@comunitea.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+from odoo import fields, models
+
+
+class StockInventory(models.Model):
+
+    _inherit = "stock.inventory.line"
+
+    # Needed beacause of odoo bug when Start a inventory in a company, when
+    # same product inventory is done in other company, and there are MORE THAN
+    # one idiom loaded in database.
+    # With this we avoid to write this field in inventory_lines belonging to
+    # other company
+    product_name = fields.Char(readonly=True)
