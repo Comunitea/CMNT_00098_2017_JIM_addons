8e721c5dbe170e6b8ea576a1ae2d7201f96e6f8d
kiko <kiko@comunitea.com>
2017-07-26 18:02:18 +0200

    [FIX] fix relates stock_global_available

--- custom_sale_order_variant_mgmt/models/sale_order.py
+++ custom_sale_order_variant_mgmt/models/sale_order.py
@@ -81,7 +81,8 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     template_line = fields.Many2one('sale.order.line.template')
-    global_available_stock = fields.Float('Stock')
+    global_available_stock = fields. \
+        Float('Stock', related='product_id.global_available_stock')
 
     @api.model
     def create(self, vals):
@@ -108,8 +109,6 @@ class SaleOrder(models.Model):
     order_line = fields.One2many(copy=False)
     sale_order_line_count = fields.Integer(
         compute='_compute_sale_order_line_count')
-    global_available_stock = fields.\
-        Float('Stock', related='product_id.global_available_stock')
 
     @api.depends('order_line')
     def _compute_sale_order_line_count(self):
