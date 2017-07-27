f5c34bfa612b0e01b3e7d2564c30e8e4e4f896d3
kiko <kiko@comunitea.com>
2017-07-27 21:08:09 +0200

    [BUG] Recalculo del peso y volumen

--- product_custom/models/product.py
+++ product_custom/models/product.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Omar Castiñeira, Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, _
+from odoo import models, fields, api,  _
 from odoo.exceptions import ValidationError
 
 
@@ -29,20 +29,18 @@ class ProductPackaging(models.Model):
 
     _inherit = "product.packaging"
 
+    @api.model
     def compute_product_dimensions(self):
         if self.qty == 0:
             raise ValidationError(_("Check quantity !!!!"))
-        weight = 0.0000
-        volume = 0.000000
-        for product in self.product_tmpl_id.product_variant_ids:
-            product.weight = self.max_weight / self.qty
-            weight += product.weight
-            product.volume = self.height * self.width * \
-                self.length / (self.qty * 1000000)
-            volume += product.volume
 
-        self.product_tmpl_id.weight = weight / self.product_variant_count
-        self.product_tmpl_id.volume = volume / self.product_variant_count
+        self.product_tmpl_id.weight = self.max_weight / self.qty
+        self.product_tmpl_id.volume = self.height * self.width * \
+                self.length / (self.qty )
+
+        for product in self.product_tmpl_id.variant_ids:
+            product.weight = self.product_tmpl_id.weight
+            product.volume = self.product_tmpl_id.volume
 
         return True
 
