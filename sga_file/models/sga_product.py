04abcb4911139c0be0b4fc0ee320bef21c7d230e
kiko <kiko@comunitea.com>
2017-08-04 19:33:47 +0200

    [FIX] Error en un if

--- sga_file/models/sga_product.py
+++ sga_file/models/sga_product.py
@@ -206,7 +206,7 @@ class SGAProductProduct(models.Model):
         print "write de product con values %s"%values
 
         if not self.sga_prod_shortdesc and not \
-            values.get('sga_prod_shortdesc', False) and sga_name_get:
+            values.get('sga_prod_shortdesc', False) and self.sga_name_get:
             values['sga_prod_shortdesc'] = self.sga_name_get[0:80]
 
         fields_to_check = ('default_code', 'barcode', 'categ_id',
