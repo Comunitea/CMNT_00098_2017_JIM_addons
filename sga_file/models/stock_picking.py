ef7a8d3314a931dddd0e766d831ec2bd62b9003c
kiko <kiko@comunitea.com>
2017-08-07 14:05:37 +0200

    [BUG] create false y calculo de confirmaciones

--- sga_file/models/stock_picking.py
+++ sga_file/models/stock_picking.py
@@ -433,7 +433,7 @@ class StockPickingSGA(models.Model):
 
                 # Si op existe, escribo qty_done, si no creo una linea de operacion con lo recibido
                 if op:# and op.product_id.id == product.id:
-                    op.qty_done += qty_done
+                    op.qty_done = qty_done
                     op.sga_changed = True
                     sga_ops_exists = True
 
