6f839ae20d14fa7ffb1c8910cc349f0d8aff8d95
jesusVMayor <jesus@comunitea.com>
2017-08-09 17:23:14 +0200

    [IMP]custom_documents: En informe de compra se quita la hora de la fecha de entrega.

---
+++ custom_documents/models/purchase.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2017 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, exceptions, _
+
+
+class PurchaseOrderLine(models.Model):
+
+    _inherit = 'purchase.order.line'
+
+    date_planned_date = fields.Date(compute='_compute_date_planned_date')
+
+    @api.depends('date_planned')
+    def _compute_date_planned_date(self):
+        for purchase in self:
+            purchase.date_planned_date = purchase.date_planned.split(' ')[0]
