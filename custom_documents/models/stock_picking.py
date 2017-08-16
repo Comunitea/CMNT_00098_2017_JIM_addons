0fcd05605fe06f691e3edcdcbdd719cdff2209c5
jesusVMayor <jesus@comunitea.com>
2017-08-16 13:33:55 +0200

    [FIX]custom_documents: Corregidas funciones para calcular fecha de envío.

--- custom_documents/models/stock_picking.py
+++ custom_documents/models/stock_picking.py
@@ -29,39 +29,37 @@ class StockPicking(models.Model):
         for pick in self:
             pick.min_date_date = pick.min_date.split(' ')[0]
 
-    @api.depends('date_done')
     # Si el albaran  se finalizó antes de las 17:30 entre semana se envía el
     # mismo día.
     def _compute_same_day_delivery(self):
-        self.ensure_one()
-        if self.date_done:
-            same_day_delivery = True
-            date_done = fields.Datetime.from_string(self.date_done)\
-                .replace(tzinfo=timezone('Etc/UTC'))\
-                .astimezone(timezone(self._context['tz']))
-            if date_done.hour > 17 or \
-                    (date_done.hour == 17 and date_done.minute > 30) or \
-                    date_done.isoweekday() in (6, 7):
-                same_day_delivery = False
-            self.same_day_delivery = same_day_delivery
+        for pick in self:
+            if pick.date_done:
+                same_day_delivery = True
+                date_done = fields.Datetime.from_string(pick.date_done)\
+                    .replace(tzinfo=timezone('Etc/UTC'))\
+                    .astimezone(timezone(pick._context['tz']))
+                if date_done.hour > 17 or \
+                        (date_done.hour == 17 and date_done.minute > 30) or \
+                        date_done.isoweekday() in (6, 7):
+                    same_day_delivery = False
+                pick.same_day_delivery = same_day_delivery
 
-    @api.depends('date_done')
     def _compute_delivery_date(self):
         # Si no se envía el mismo día se comprueba que el día de envío no
         # sea ni sabado ni domingo
-        self.ensure_one()
-        if self.date_done:
-            if self.same_day_delivery:
-                self.delivery_date = self.date_done
-            else:
-                date_done = fields.Datetime.from_string(self.date_done)
-                next_date = date_done + timedelta(days=1)
-                delivery_date = next_date
-                if next_date.isoweekday() == 6:
-                    delivery_date = next_date + timedelta(days=2)
-                elif next_date.isoweekday() == 7:
-                    delivery_date = next_date + timedelta(days=1)
-                self.delivery_date = delivery_date
+        for pick in self:
+            if pick.date_done:
+                if pick.same_day_delivery:
+                    pick.delivery_date = pick.date_done
+                else:
+                    date_done = fields.Datetime.from_string(pick.date_done)
+                    next_date = date_done + timedelta(days=1)
+                    delivery_date = next_date
+                    if next_date.isoweekday() == 6:
+                        delivery_date = next_date + timedelta(days=2)
+                    elif next_date.isoweekday() == 7:
+                        delivery_date = next_date + timedelta(days=1)
+                    pick.delivery_date = delivery_date
 
     @api.multi
     def _compute_delivery_amount(self):
