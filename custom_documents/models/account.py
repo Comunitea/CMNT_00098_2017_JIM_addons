eb3cb36affdf771b52e331eab12f6b2b746dcd7a
jesusVMayor <jesus@comunitea.com>
2017-08-22 09:48:11 +0200

    [IMP]custom_documents: Añadido descuento global a facturas.

--- custom_documents/models/account.py
+++ custom_documents/models/account.py
@@ -2,7 +2,7 @@
 # © 2017 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, fields
+from openerp import models, api, fields, _
 
 
 class AccountInvoice(models.Model):
@@ -11,6 +11,35 @@ class AccountInvoice(models.Model):
 
     global_discount_amount = fields.Monetary(
         compute='_compute_global_discount_amount')
+    global_discount_percentage = fields.Float('Global discount')
+
+    @api.multi
+    def apply_global_discount_percentage(self):
+        self.ensure_one()
+        self.invoice_line_ids.filtered(lambda x: x.global_disc_line).unlink()
+        if not self.global_discount_percentage:
+            return True
+        new_line_vals = {
+            'product_id': self.env.ref('commercial_rules.product_discount').id,
+            'name': _('Global discount'),
+            'promotion_line': True,
+            'global_disc_line': True,
+            'quantity': 1,
+            'account_id': self.invoice_line_ids.with_context(
+                journal_id=self.journal_id.id)._default_account(),
+            'uom_id': False,
+            'invoice_line_tax_ids': False
+        }
+        new_invoice_line = self.env['account.invoice.line'].new(new_line_vals)
+        new_invoice_line._onchange_product_id()
+        vals = new_invoice_line._convert_to_write(
+                new_invoice_line._cache)
+        vals.update(new_line_vals)
+        vals['price_unit'] = self.amount_untaxed * \
+            (self.global_discount_percentage / 100.0)
+        self.write({'invoice_line_ids': [(0, 0, vals)]})
+
+        return True
 
     @api.multi
     def _compute_global_discount_amount(self):
@@ -23,21 +52,23 @@ class AccountInvoice(models.Model):
             else:
                 invoice.global_discount_amount = 0.0
 
+
 class AccountInvoiceLine(models.Model):
 
     _inherit = 'account.invoice.line'
 
     name_report = fields.Char(compute='_compute_name_report')
     promotion_line = fields.Boolean("Rule Line")
+    global_disc_line = fields.Boolean()
 
     @api.multi
     def _compute_name_report(self):
         for line in self:
             name_report = line.name
             if '[%s]' % line.product_id.default_code in line.name:
-                name_report = line.name.replace('[%s]' % line.product_id.default_code, '')
+                name_report = line.name.replace(
+                    '[%s]' % line.product_id.default_code, '')
             line.name_report = name_report
-
 
     @api.multi
     def get_custom_qty_price(self, qty):
