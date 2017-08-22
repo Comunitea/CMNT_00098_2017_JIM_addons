f8622d1b9f05267918f5c96b87f501279fd6095f
Roodin <santi@comunitea.com>
2017-08-23 00:44:01 +0200

    [FIX]. jim_stock. Correcion del contexto para el calculo del stock en la vista lista

--- jim_stock/models/product.py
+++ jim_stock/models/product.py
@@ -138,8 +138,9 @@ class ProductProduct(models.Model):
             self.env['stock.location'].search([('deposit', '=', True)]).ids
         company_ids = \
             self.env['res.company'].search([('no_stock', '=', True)]).ids
-        ctx = self._context.copy()
+
         for product in self:
+            ctx = self._context.copy()
             deposit_real_stock = 0
             deposit_available_stock = 0
             sale_lines_stock = 0
