c15d50a10584c720b3b2bcb970d456079e38543d
kiko <kiko@comunitea.com>
2017-08-25 13:09:16 +0200

    [IMP] Añadir estado del estado LQDR

--- jim_sale/models/sale.py
+++ jim_sale/models/sale.py
@@ -29,6 +29,7 @@ class SaleOrder(models.Model):
     work_to_do = fields.Text('Trabajo a realizar')
     route_id = fields.Many2one('stock.location.route', string='Force Route',
                                domain=[('sale_selectable', '=', True)])
+    lqdr_state = fields.Selection([('lqdr_no','LQDR No tramitado'), ('lqdr_si','LQDR Tramitado')], string="Estado LQDR", default='lqdr_no')
 
     @api.model
     def create_web(self, vals):
