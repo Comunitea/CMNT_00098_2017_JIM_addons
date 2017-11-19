da4d35e55bf3bf0d85ce7ab4f4cf447fe061e039
kiko <kiko@comunitea.com>
2017-11-19 21:59:56 +0100

    [FIX] Error en vista

--- jim_rma_intercompany/wizard/__init__.py
+++ jim_rma_intercompany/wizard/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # © 2016 Comunitea - Kiko Sánchez <kiko@comunitea.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-import crm_claim_rma_make_refund, claim_make_picking, crm_claim_rma_make_batch_refund
+import claim_make_picking, crm_claim_rma_make_batch_refund
