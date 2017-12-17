cd5897267dc12ea8f349ecd029f497ec977294c5
kiko <kiko@comunitea.com>
2017-12-17 20:48:32 +0100

    [IMP] Herramientas para inventario (quants, ubicaciones ...). Reorganización bajo menu

--- regularize_stock_location/wizard/regularize_location.py
+++ regularize_stock_location/wizard/regularize_location.py
@@ -7,24 +7,125 @@
 from odoo import fields, models
 
 
-class StockLocation(models.TransientModel):
+class WizRegularizeLocation(models.TransientModel):
 
     _name = "wiz.regularize.location"
 
     location_id = fields.Many2one('stock.location', string="Location to regularize")
+    product_id = fields.Many2one('product.product', string="Product to regularize")
+    action_done = fields.Boolean('Auto validation', default=False)
+
+    def get_inventory_for(self, product_id, location_id, company_id):
+
+        # miro si hay un inventario abierto para esta compañia y si no lo creo
+        domain = [('location_id', '=', location_id.id),
+                  ('company_id', '=', company_id.id),
+                  ('filter', '=', 'partial'),
+                  ('state', '=', 'confirm')]
+        inventory = self.env['stock.inventory'].search(domain)
+
+        if not inventory:
+            stock_inv_vals = {
+                'name': u'%s (%s)' % ("MECALUX / ", company_id.ref),
+                'location_id': location_id.id,
+                'filter': 'partial',
+                'company_id': company_id.id,
+            }
+            inventory = self.env['stock.inventory'].sudo().create(stock_inv_vals)
+        inventory = inventory[0]
+        inventory.action_start()
+        return inventory
+
+    def new_inv_line(self, product_id, qty, inventory_id):
+
+        # Si hay alguno pendiente lo borro
+        domain = [('product_id', '=', product_id),
+                  ('location_id', '=', inventory_id.location_id.id),
+                  ('company_id', '=', inventory_id.company_id.id),
+                  ('state', 'in', ('draft', 'confirm'))]
+        inventory_line = self.env['stock.inventory.line'].search(domain)
+
+        if inventory_line:
+            inventory_line.unlink()
+        new_line_vals = {
+            'product_id': product_id,
+            'product_qty': qty,
+            'inventory_id': inventory_id.id,
+            'company_id': inventory_id.company_id.id,
+            'location_id': inventory_id.location_id.id,
+        }
+        new_inventory_line = self.env['stock.inventory.line'].sudo().create(new_line_vals)
+        return new_inventory_line
+
 
     def regularize(self):
 
+        if not self.location_id:
+            raise ValueError ('Need a location to regularize')
         self.ensure_one()
         loc_id = self.location_id.id
         domain = [('company_id', '=', self.env.user.company_id.id),
                   ('reservation_id', '=', False),
                   ('location_id', '=', loc_id)
                   ]
-        domain += [('qty', '<', 2)]
-        domain += [('qty', '>', -2)]
+        if self.product_id:
+            domain += [('product_id', '=', self.product_id.id)]
+        inventory = self.get_inventory_for(self.product_id, self.location_id, self.env.user.company_id)
+
+
+        flds = ['product_id', 'qty']
+        quants = self.env['stock.quant'].read_group(domain, flds, 'product_id')
+        quants = [quant for quant in quants if quant['qty'] != 0]
+
+        #quants = dict((item['product_id'][0], item['qty']) for item in
+        #     self.env['stock.quant'].read_group(domain, flds, 'product_id') if item['qty'] != 0)
+        cont = len(quants)
+
+        for quant in quants:
+            qty = quant['qty']
+            product_id = quant['product_id'][0]
+            new_inventory_line = self.new_inv_line(product_id, 0, inventory)
+
+        if self.action_done:
+            inventory.action_done()
+
+        view = self.env.ref('stock'
+                            '.view_inventory_tree')
+        view_form = self.env.ref('stock'
+                                 '.view_inventory_form')
+
+        res = {
+            'name': 'Inventories',
+            'view_type': 'form',
+            'view_mode': 'tree,form',
+            'domain': unicode([('id', 'in', inventory.ids)]),
+            'res_model': 'stock.inventory',
+            'res_id': inventory.id,
+            'type': 'ir.actions.act_window',
+            'view_id': False,
+            'views': [(view.id, 'tree'), (view_form.id, 'form')]
+        }
+        return res
+
+
+
+
+    def regularize_bis(self):
+        if not self.location_id:
+            raise ValueError ('Need a location to regularize')
+        self.ensure_one()
+        loc_id = self.location_id.id
+        domain = [('company_id', '=', self.env.user.company_id.id),
+                  ('reservation_id', '=', False),
+                  ('location_id', '=', loc_id)
+                  ]
+        if self.product_id:
+            domain += [('product_id', '=', self.product_id.id)]
+        #domain += [('qty', '<', 2)]
+        # domain += [('qty', '>', -2)]
+        limit = 20
         location_regularize = 29
-        quants = self.env['stock.quant'].search(domain, order='qty asc')
+        quants = self.env['stock.quant'].search(domain, order='id desc, qty asc', limit=limit)
 
         pick_vals = self.get_reg_pick_val(location_regularize, loc_id)
 
@@ -84,11 +185,17 @@ class StockLocation(models.TransientModel):
         return vals
 
     def regularize_quants(self):
+
+        if not self.location_id:
+            raise ValueError ('Necesitas poner una ubicacion')
         loc_id = self.location_id.id
         domain = [('company_id', '=', self.env.user.company_id.id),
                   ('reservation_id', '=', False),
                   ('location_id', '=', loc_id)
                   ]
+        if self.product_id:
+            domain += [('product_id', '=', self.product_id.id)]
+
 
         location_regularize = 29
         flds=['product_id', 'qty']
