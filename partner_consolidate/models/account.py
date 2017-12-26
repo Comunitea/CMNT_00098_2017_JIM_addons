584ed5fc00c5ecc2f61cb6bc22c288ce2e69f5d4
Roodin <santi@comunitea.com>
2017-12-26 19:34:17 +0100

    [FIX] partner_consolidate. Corrige llamada en on_change para devolver correctamente la posición fiscal

--- partner_consolidate/models/account.py
+++ partner_consolidate/models/account.py
@@ -12,35 +12,36 @@ class AccountInvoice(models.Model):
     @api.depends('partner_id', 'partner_id.consolidate')
     def _get_commercial_partner(self):
 
-        for inv_sudo in self:
-            inv = self.with_context(force_company=inv_sudo.company_id.id).\
-                browse(inv_sudo.id)
+        for inv in self:
             if inv.state and inv.state != 'draft':
                 self._cr.execute("""SELECT commercial_partner_id
                                     FROM account_invoice
                                     WHERE id = %s""" % str(inv.id))
                 old_value = self._cr.fetchall()
                 if old_value and old_value[0]:
-                   inv_sudo.commercial_partner_id = old_value[0][0]
+                   inv.commercial_partner_id = old_value[0][0]
                 continue
-            if inv.partner_id.consolidate and inv.partner_id.parent_id:
+
+            partner_in_company = inv.partner_id.with_context(
+                force_company=inv.company_id.id)
+            if partner_in_company.consolidate and partner_in_company.parent_id:
                 commercial_partner_id = \
-                    inv.partner_id.parent_id.commercial_partner_id
+                    partner_in_company.parent_id.commercial_partner_id
             else:
-                if not inv.partner_id.parent_id:
+                if not partner_in_company.parent_id:
                     commercial_partner_id = \
-                        inv.partner_id.commercial_partner_id
+                        partner_in_company.commercial_partner_id
                 else:
-                    if inv.partner_id.parent_id.consolidate and \
-                            inv.partner_id.parent_id.parent_id:
+                    if partner_in_company.parent_id.consolidate and \
+                            partner_in_company.parent_id.parent_id:
                         commercial_partner_id = \
-                            inv.partner_id.parent_id.parent_id
+                            partner_in_company.parent_id.parent_id
                     else:
                         commercial_partner_id = \
-                            inv.partner_id.commercial_partner_id
+                            partner_in_company.commercial_partner_id
 
-            inv_sudo.commercial_partner_id = commercial_partner_id
-            inv_sudo.fiscal_position_id = \
+            inv.commercial_partner_id = commercial_partner_id
+            inv.fiscal_position_id = \
                 commercial_partner_id.property_account_position_id
 
     @api.model
