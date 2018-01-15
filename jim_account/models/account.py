86e07729821c2927b1a9c3506729f9ef405b7fe8
Roodin <santi@comunitea.com>
2018-01-15 07:13:39 +0100

    [FIX] jim_account. Edicion de aranceles en facturas de proveedor validadas

--- jim_account/models/account.py
+++ jim_account/models/account.py
@@ -39,7 +39,7 @@ class AccountInvoice(models.Model):
     user_id = fields.Many2one(states={'draft': [('readonly', False)],
                                       'open': [('readonly', False)],
                                       'paid': [('readonly', False)]})
-    #invoice_line_ids = fields.One2many(readonly=False)
+    invoice_line_ids = fields.One2many(readonly=False)
 
     @api.multi
     def action_invoice_paid(self):
@@ -47,33 +47,22 @@ class AccountInvoice(models.Model):
         return super(AccountInvoice, super_invoices).action_invoice_paid()
 
 
-# class AccountInvoiceLine(models.Model):
-#
-#     _inherit = 'account.invoice.line'
-#
-#     name = fields.Char(required=True, states={'open': [('readonly', True)]})
-#     uom_id = fields.Many2one(states={'open': [('readonly', True)]})
-#     product_id = fields.Many2one(states={'open': [('readonly', True)]})
-#     price_unit = fields.Float(states={'open': [('readonly', True)]})
-#     quantity = fields.Float(states={'open': [('readonly', True)]})
-#     discount = fields.Float(states={'open': [('readonly', True)]})
-#     chained_discount = fields.Float(states={'open': [('readonly', True)]})
-#     invoice_line_tax_ids = fields.Many2many(states={'open': [('readonly', True)]})
-#     account_analytic_id = fields.Many2one(states={'open': [('readonly', True)]})
-#     asset_category_id = fields.Many2one(states={'open': [('readonly', True)]})
-#     account_id = fields.Many2one(states={'open': [('readonly', True)]})
-#     analytic_tag_ids = fields.Many2many(states={'open': [('readonly', True)]})
-#     state = fields.Selection([
-#             ('draft','Draft'),
-#             ('proforma', 'Pro-forma'),
-#             ('proforma2', 'Pro-forma'),
-#             ('open', 'Open'),
-#             ('paid', 'Paid'),
-#             ('cancel', 'Cancelled'),
-#         ], related='invoice_id.state')
-#
-#     @api.multi
-#     def unlink(self):
-#         if any(self.filtered(lambda r: r.state in ('open', 'paid'))):
-#             raise UserError(_('No se pueden eliminar lineas de facturas abiertas'))
-#         return super(AccountInvoiceLine, self).unlink()
+class AccountInvoiceLine(models.Model):
+
+    _inherit = 'account.invoice.line'
+
+    name = fields.Char(required=True, states={'open': [('readonly', True)]})
+    state = fields.Selection([
+            ('draft','Draft'),
+            ('proforma', 'Pro-forma'),
+            ('proforma2', 'Pro-forma'),
+            ('open', 'Open'),
+            ('paid', 'Paid'),
+            ('cancel', 'Cancelled'),
+        ], related='invoice_id.state')
+
+    @api.multi
+    def unlink(self):
+        if any(self.filtered(lambda r: r.state in ('open', 'paid'))):
+            raise UserError(_('No se pueden eliminar lineas de facturas abiertas'))
+        return super(AccountInvoiceLine, self).unlink()
