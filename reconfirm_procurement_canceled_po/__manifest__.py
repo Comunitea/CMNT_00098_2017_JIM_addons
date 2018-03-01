7de959de6fde83759874b5ee1266ab3c3a96f270
kiko <kiko@comunitea.com>
2018-03-01 22:36:25 +0100

    [ADD] reconfirm_procurement_canceled_po Modulo para reconfirmar alabranes de dropshipping

---
+++ reconfirm_procurement_canceled_po/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea Servicios Tecnologicos (<http://www.comunitea.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+{
+    'name': 'Reconfirm procurement in canceled po',
+    'version': '10.0',
+    'author': 'Comunitea, ',
+    "category": "",
+    'license': 'AGPL-3',
+    'description': 'Allow reconfirm procurement in purchase orders',
+    'depends': ['purchase'],
+    'contributors': [
+        "Comunitea",
+        "Kiko Sanchez<kiko@comunitea.com>"]
+    ,
+
+    "data": [
+        'wizard/wzd_confirm_procurement.xml',
+        #'security/ir.model.access.csv',
+    ],
+    "demo": [
+
+    ],
+    'test': [
+
+    ],
+    'installable': True
+}
+
