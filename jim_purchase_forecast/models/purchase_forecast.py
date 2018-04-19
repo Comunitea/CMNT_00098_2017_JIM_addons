4755a50c04881e9f1d834640e02520d7f3c5fa7b
javierjcf <javierjcf@gmail.com>
2018-04-19 19:21:43 +0200

    [REF]jim_purchase_forecast: Refactorización cálculos a producto

--- jim_purchase_forecast/models/purchase_forecast.py
+++ jim_purchase_forecast/models/purchase_forecast.py
@@ -3,8 +3,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import fields, models, api, exceptions, _
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import numpy as np
 
 
@@ -56,91 +54,6 @@ class PurchaseForecast(models.Model):
         if demand < 0:
             demand = 0
         return demand
-
-    @api.model
-    def _get_sales(self, product_id):
-        query = """
-                    SELECT  Sum(
-                    case WHEN product_uom_qty - qty_delivered >= 0
-                      THEN product_uom_qty - qty_delivered
-                      ELSE 0 END)
-                     FROM sale_order_line WHERE
-                      product_id = %s
-                      and state in ('lqdr','pending', 'progress',
-                       'progress_lqdr','sale')
-                    GROUP BY product_id
-                """ % (product_id)
-
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            qty = qres[0][0]
-            return qty
-        return 0
-
-    @api.multi
-    def get_dates(self, num_years_ago=0):
-        today = datetime.today()
-        future_date = today + relativedelta(months=+self.stock_months)
-
-        year_today = today.year - num_years_ago
-        year_future = future_date.year - num_years_ago
-        month_today = today.strftime("%m")
-        month_future = future_date.strftime("%m")
-
-        day_today = today.strftime("%d")
-        day_future = future_date.strftime("%d")
-
-        date_start = str(year_today) + '-' + month_today + '-' + day_today
-        date_end = str(year_future) + '-' + month_future + '-' + day_future
-        return date_start, date_end
-
-    @api.multi
-    def _get_qty_year_ago(self, product_id, num_years_ago=0):
-        self.ensure_one()
-        qty = 0
-        date_start, date_end = self.get_dates(num_years_ago)
-        print date_start
-        print date_end
-        company_id = self.env.user.company_id.id
-
-        # Query sale hystory
-        query = """
-            SELECT sum(qty)
-            FROM sale_history
-            WHERE date >= '%s' AND
-                  date <= '%s' AND product_id = %s AND company_id = %s
-            GROUP BY product_id
-        """ % (date_start, date_end, product_id, company_id)
-
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            qty = qres[0][0]
-            if not qty:
-                qty = 0
-
-        # Query odoo sales from outgoings moves
-        qty2 = 0
-        query2 = """
-            SELECT sum(product_uom_qty)
-            FROM stock_move sm
-            INNER JOIN stock_picking sp on sp.id = sm.picking_id
-            INNER JOIN stock_picking_type spt on spt.id = sp.picking_type_id
-            INNER JOIN stock_location sl on sl.id = sp.location_dest_id
-            WHERE sm.product_id = %s  AND
-                  sm.date_expected >= '%s' AND sm.date_expected <= '%s' AND
-                  sp.state in ('done') AND
-                  spt.code = 'outgoing' AND sl.usage = 'customer' AND
-                  sp.partner_id not in (select partner_id from res_company)
-        """ % (product_id, date_start, date_end)
-        self._cr.execute(query2)
-        qres = self._cr.fetchall()
-        if qres:
-            qty2 = qres[0][0]
-            if not qty2:
-                qty2 = 0
-        return qty + qty2
 
     @api.multi
     def _query_product_category(self):
@@ -235,159 +148,33 @@ class PurchaseForecast(models.Model):
                                           lines created'))
         return self.env['product.product'].browse(product_ids)
 
-    def _get_incoming_stock_months(self, product_id):
-        """
-        Get purchase qty in stock motns range
-        """
-        date_start, date_end = self.get_dates()
-        query = """
-            SELECT sum(product_uom_qty)
-            FROM stock_move sm
-            INNER JOIN stock_picking sp on sp.id = sm.picking_id
-            INNER JOIN stock_picking_type spt on spt.id = sp.picking_type_id
-            INNER JOIN stock_location sl on sl.id = sp.location_id
-            WHERE sm.product_id = %s  AND
-                  sm.date_expected >= '%s' AND sm.date_expected <= '%s' AND
-                  sp.state in ('assigned', 'partially_available') AND
-                  spt.code = 'incoming' AND sl.usage = 'supplier' AND
-                  sp.partner_id not in (select partner_id from res_company)
-        """ % (product_id, date_start, date_end)
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            qty = qres[0][0]
-            return qty
-        return 0
-
-    def _get_incoming_remaining(self, product_id):
-        """
-        Get purchase out of stock moths range
-        """
-        date_start, date_end = self.get_dates()
-        query = """
-            SELECT sum(product_uom_qty)
-            FROM stock_move sm
-            INNER JOIN stock_picking sp on sp.id = sm.picking_id
-            INNER JOIN stock_picking_type spt on spt.id = sp.picking_type_id
-            INNER JOIN stock_location sl on sl.id = sp.location_id
-            WHERE sm.product_id = %s  AND
-                  sm.date_expected < '%s' AND sm.date_expected > '%s' AND
-                  sp.state in ('assigned', 'partially_available') AND
-                  spt.code = 'incoming' AND sl.usage = 'supplier' AND
-                  sp.partner_id not in (select partner_id from res_company)
-        """ % (product_id, date_start, date_end)
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            qty = qres[0][0]
-            return qty
-        return 0
-
-    def _get_pending_purchase(self, product_id):
-        """
-        Get purchase out of stock moths range
-        """
-        qty = 0
-        query = """
-            SELECT sum(product_qty)
-            FROM purchase_order_line pol
-            INNER JOIN purchase_order po on po.id = pol.order_id
-            WHERE pol.product_id = %s  AND
-                  po.state not in ('cancel', 'purchase', 'done')
-        """ % (product_id)
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            qty = qres[0][0]
-            if not qty:
-                qty = 0
-        return qty
-
-    def _get_related_purchase_id(self, product_id):
-        """
-        Get purchase out of stock moths range
-        """
-        po_id = False
-        query = """
-            SELECT min(po.id)
-            FROM purchase_order_line pol
-            INNER JOIN purchase_order po on po.id = pol.order_id
-            WHERE pol.product_id = %s  AND
-                  po.state not in ('cancel', 'purchase', 'done')
-        """ % (product_id)
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            po_id = qres[0][0]
-            if not po_id:
-                po_id = False
-        return po_id
-
-    def _get_related_picking_id(self, product_id):
-        """
-        Get purchase out of stock moths range
-        """
-        picking_id = False
-        query = """
-            SELECT min(sp.id)
-            FROM stock_move sm
-            INNER JOIN stock_picking sp on sp.id = sm.picking_id
-            INNER JOIN stock_picking_type spt on spt.id = sp.picking_type_id
-            INNER JOIN stock_location sl on sl.id = sp.location_id
-            WHERE sm.product_id = %s  AND
-                  sp.state in ('assigned', 'partially_available') AND
-                  spt.code = 'incoming' AND sl.usage = 'supplier' AND
-                  sp.partner_id not in (select partner_id from res_company)
-        """ % (product_id)
-        self._cr.execute(query)
-        qres = self._cr.fetchall()
-        if qres:
-            pick_id = qres[0][0]
-            if pick_id:
-                picking_id = pick_id
-        return picking_id
-
     @api.multi
     def create_lines(self):
         self.ensure_one()
         self.delete_forecast_lines()
         products = self._get_products()
         for product in products:
-            ventas = self._get_sales(product.id)
-            vals = {
-                'forecast_id': self.id,
-                'product_id': product.id,
-                'year1_ago': self._get_qty_year_ago(product.id, 1),
-                'year2_ago': self._get_qty_year_ago(product.id, 2),
-                'year3_ago': self._get_qty_year_ago(product.id, 3),
-                'year4_ago': self._get_qty_year_ago(product.id, 4),
-                'year5_ago': self._get_qty_year_ago(product.id, 5),
-                'sales': ventas,
-                'incoming_months': self._get_incoming_stock_months(product.id),
-                'incoming_remaining': self._get_incoming_remaining(product.id),
-                'pending_purchase': self._get_pending_purchase(product.id),
-                'related_purchase_id':
-                self._get_related_purchase_id(product.id),
-                'related_picking_id':
-                self._get_related_picking_id(product.id),
-                'stock': product.global_real_stock
-            }
-            line = self.env['purchase.forecast.line'].create(vals)
-            line.demand = self._get_demand(line)
-            purchase = max(line.demand, ventas) - product.global_real_stock \
-                - line.incoming_months
+            line_vals = product._get_forecast_line_vals(self.stock_months)
+            line_vals['forecast_id'] = self.id
+            # ventas = line_vals['sales']
+            line = self.env['purchase.forecast.line'].create(line_vals)
+
+            #TODO IMPROVE WRITING EFFICIENT
+            line.demand = product._get_demand(line_vals)
+            purchase = product._get_purchase(line.demand, line_vals)
+
             line.seller_id = product.seller_ids and product.seller_ids[0] \
                 and product.seller_ids[0].name or False
             line.seller_price = line.seller_id and product.seller_ids[0].price
             line.seller2_id = product.seller_ids and len(product.seller_ids)\
-                              > 1 and product.seller_ids[1] \
-                             and product.seller_ids[1].name or False
+                > 1 and product.seller_ids[1] \
+                and product.seller_ids[1].name or False
             line.seller2_price = line.seller2_id and product.seller_ids[
                 1].price
             line.harbor_id = line.seller_id and line.seller_id.harbor_ids and\
                 line.seller_id.harbor_ids[0] or False
             line.harbor2_id = line.seller2_id and line.seller2_id.harbor_ids \
-                              and line.seller2_id.harbor_ids[0] or False
+                and line.seller2_id.harbor_ids[0] or False
 
             if purchase < 0:
                 purchase = 0
