0b50e952af48f88dcb2f5952256ebd136014fc3c
Kiko Sánchez <kiko@comunitea.com>
2019-04-25 14:02:52 +0200

    [DEV] product_ean_generator Añado limite a la secuencia

---
+++ product_ean_generator/models/ir_sequence.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Kiko Sánchez, Comunitea Servicios Tecnológicos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class IrSequence(models.Model):
+    _inherit = "ir.sequence"
+
+    upper_limit = fields.Integer(string='Limit', help="Upper limit for this sequence")
+
+    def _next_do(self):
+        if self.implementation == 'standard' and self.upper_limit < self.number_next_actual:
+             raise ValidationError('Limit sequence')
+        return super(IrSequence, self)._next_do()
