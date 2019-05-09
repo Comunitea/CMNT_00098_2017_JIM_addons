964d6540326c1c3a951a22135e4381f6b960def5
Pablo <31565658+pablo-lp@users.noreply.github.com>
2019-05-09 14:22:12 +0200

    Módulos Jim Sports

---
+++ js_update_prices/controllers/main.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+class jsUpdatePrices(http.Controller):
+    @http.route([
+        '/js_update_prices'
+    ], type='http', auth='user')
+    def index(self, **kw):
+        return request.render('js_update_prices.prices_edit_index')
+
+    @http.route([
+        '/js_update_prices/run'
+    ], type='http', auth='user')
+    def run(self, tmode=0, order='', **kw):
+
+        # Solo pueden acceder usuarios con permisos de configuración (Administración/Ajustes)
+        if request.env.user.has_group('base.group_system'):
+
+            numLines = 0 # Contador para numero de líneas
+            test_mode = int(tmode) # Test mode como entero (0-1)
+            debug_processed = [] # Creamos una lista para guardar los valores
+
+            # Hacemos uso de request para acceder al modelo de los pedidos
+            if (order != ''):
+                purchases = request.env['sale.order'].sudo().search([
+                    ('name', '=', order)
+                ], order='id')
+            else:
+                purchases = request.env['sale.order'].sudo().search([
+                    ('state', '=', 'draft')
+                ], order='id')
+
+            #import web_pdb; web_pdb.set_trace()
+
+            # Realizamos un bucle para descargar las imágenes y guardar los resultados
+            for order in purchases:
+
+                # Realizamos un bucle para recorrer las lineas del pedido
+                for line in order.order_line:
+
+                    numLines += 1
+
+                    # Buscamos los precios de compra
+                    supplier_prices = request.env['product.supplierinfo'].sudo().search([
+                        ('company_id', '=', line.company_id.id),
+                        ('product_id', '=', line.product_id.id)
+                    ], order='sequence')
+
+                    # Si la linea no está facturada actualizamos el importe
+                    if (line.state == 'sale' and (line.invoice_status == 'no' or line.invoice_status == 'to invoice')):
+
+                        old_price = line.price_unit
+
+                        # Si no estamos en modo test
+                        if not test_mode:
+
+                            # Actualizamos el precio
+                            line.product_id_change()
+
+                        if (line.price_unit != old_price):
+
+                            # Guardamos la linea en el listado
+                            debug_processed.append({
+                                'id': line.id,
+                                'name': line.name,
+                                'quantity': line.product_qty,
+                                'old_price': old_price,
+                                'new_price': line.price_unit
+                            })
+
+            # Pasamos los resultados a la vista
+            return request.render('js_update_prices.prices_edit_batch', {
+                'total': numLines,
+                'total_changes': len(debug_processed),
+                'products': debug_processed,
+                'test_mode': test_mode
+            })
+
+        else:
+            return 'No está autorizado para acceder a esta página'
