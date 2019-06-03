2ed946555a92bb8fd415c5702d5f57ea5f6c39a6
Pablo <31565658+pablo-lp@users.noreply.github.com>
2019-06-03 17:49:25 +0200

    Eliminar módulos de Jim Sports de rama master

--- js_addons/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2018 - TODAY, Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import res_partner
-from . import product
-from . import sale
--- js_addons/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    group_companies_ids = fields.Many2many('res.company', string='Web Access')
