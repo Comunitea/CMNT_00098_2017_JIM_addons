89dd014e3c19e97142fa4d6bd20b6868377e6e43
Pablo <31565658+pablo-lp@users.noreply.github.com>
2019-06-21 09:46:12 +0200

    Correcciones strings y búsqueda de campos

--- js_categorization/models/categorization.py
+++ js_categorization/models/categorization.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 from odoo.exceptions import AccessError, UserError
-#import xml.etree.ElementTree as xee
 import lxml.etree as xee
 import unidecode
 import re
 
 class CategorizationType(models.Model):
     _name = 'js_categorization.type'
-    _description = "Categorization types"
+    _description = "Categorization Types"
     _sql_constraints = [('categorization_type_unique', 'unique(name)', 'Type must be unique in categorization!')]
     _order = 'sequence, name, id'
     name = fields.Char(required=True, translate=False)
@@ -35,7 +34,7 @@ class CategorizationType(models.Model):
 
 class CategorizationField(models.Model):
     _name = 'js_categorization.field'
-    _description = 'Categorization fields'
+    _description = 'Categorization Fields'
     _inherit = 'ir.model.fields'
     _order = 'sequence, field_description, id'
 
@@ -56,14 +55,14 @@ class CategorizationField(models.Model):
     @api.model
     def _get_field_types(self):
         return [
-            ('char', 'Texto'),
-            ('date', 'Fecha'),
-            ('float', 'Decimal'),
-            ('monetary', 'Moneda'),
-            ('integer', 'Entero'),
-            ('many2one', 'Selección'), # Special selection for categorization values
-            ('many2many', 'Multi-selección'), # Special multi-selection for categorization values
-            ('text', 'Texto largo')
+            ('char', _('Text')),
+            ('text', _('Long Text')),
+            ('date', _('Date')),
+            ('float', _('Decimal')),
+            ('monetary', _('Monetary')),
+            ('integer', _('Integer')),
+            ('many2one', _('Select')), # Special selection for categorization values
+            ('many2many', _('Multiselect')) # Special multi-selection for categorization values
         ]
 
     @api.onchange('field_description')
@@ -90,19 +89,26 @@ class CategorizationField(models.Model):
 
     @api.model
     def _createFieldsXml(self):
+        # import web_pdb; web_pdb.set_trace()
         categorization_generic_type = self.env.ref('js_categorization.generic_type')
         categorization_product_view = self.env.ref('js_categorization.categorization_product_form_view').sudo()
         categorization_variant_view = self.env.ref('js_categorization.categorization_variant_form_view').sudo()
+        categorization_search_view = self.env.ref('js_categorization.categorization_search_by_field').sudo()
         # Parse XML string
         parser = xee.XMLParser(remove_blank_text=True)
         pdoc = xee.fromstring(categorization_product_view.arch_base, parser=parser)
         vdoc = xee.fromstring(categorization_variant_view.arch_base, parser=parser)
+        sdoc = xee.fromstring(categorization_search_view.arch_base, parser=parser)
         # Get fields container
         pdoc_categorization_section = pdoc.find('.//div[@id="categorization_fields"]/group')
         vdoc_categorization_section = vdoc.find('.//div[@id="categorization_fields"]/group')
         # Remove all sub-elements (childs)
         pdoc_categorization_section.clear()
         vdoc_categorization_section.clear()
+        sdoc.clear()
+        # Create search view xpath attrs, sdoc have a base xpath
+        sdoc.set('expr', "//search")
+        sdoc.set('position', "inside")
         # Regenerate fields XML for each type
         for type in self.env['js_categorization.type'].search([]):
             # Get fields for this type
@@ -120,44 +126,42 @@ class CategorizationField(models.Model):
                     vdoc_field_group.set('attrs', "{ 'invisible': [('categorization_type', '!=', %d)] }" % (type.id))
                 # Create fields XML
                 for field in fields_for_type:
-                    if field.model_id.model == 'product.template':
+                    if field.model_id.model == 'product.template': # If is a product template
                         doc_field_input = xee.SubElement(pdoc_field_group, 'field')
-                    else:
+                    else: # If is a product variant
                         doc_field_input = xee.SubElement(vdoc_field_group, 'field')
                     doc_field_input.set('name', field.name)
                     doc_field_input.set('string', field.field_description)
-                    if field.ttype == 'many2many':
+                    if field.ttype == 'many2many': # If is many2many field
                         doc_field_input.set('widget', 'many2many_tags')
-                    if field.ttype in ('many2one', 'many2many'):
+                    if field.ttype in ('many2one', 'many2many'): # If is many2one or many2many field
                         doc_field_input.set('options', "{'no_open': True, 'no_create': True}")
                         doc_field_input.set('domain', "[('id', 'in', %s)]" % (str(field.selection_vals.ids)))
+                    if field.index: # If is indexed field
+                        doc_field_search = xee.SubElement(sdoc, 'field')
+                        doc_field_search.set('name', field.name)
+                        doc_field_search.set('string', field.field_description)
         # Save XML to database
-        # import web_pdb; web_pdb.set_trace()
         categorization_product_view.arch_base = xee.tostring(pdoc, pretty_print=True)
         categorization_variant_view.arch_base = xee.tostring(vdoc, pretty_print=True)
+        categorization_search_view.arch_base = xee.tostring(sdoc, pretty_print=True)
 
     @api.model
     def _resetXml(self):
-        for view in ('js_categorization.categorization_product_form_view', 'js_categorization.categorization_variant_form_view'):
+        for view in ('categorization_search_by_field', 'categorization_product_form_view', 'categorization_variant_form_view'):
             # Get view object
-            categorization_view = self.env.ref(view).sudo()
+            categorization_view = self.env.ref('js_categorization.' + view).sudo()
             # Get and parse view XML
             doc = xee.fromstring(categorization_view.arch_base)
-            # Delete all childs
-            doc_cat_section = doc.find('.//div[@id="categorization_fields"]/group').clear()
+            # In search view clear all doc and set xpath
+            if (view == 'categorization_search_by_field'):
+                doc.clear()
+                doc.set('expr', "//search")
+                doc.set('position', "inside")
+            else: # On others delete group childs
+                doc.find('.//div[@id="categorization_fields"]/group').clear()
             # Save to field
             categorization_view.arch_base = xee.tostring(doc)
-
-    @api.model
-    def _checkFieldRestrictions(self, vals):
-        # This restrictions are copied from base module, file ir.model.py, method create()
-        # It is necessary to check this before do nothing
-        if vals.get('state', 'manual') == 'manual':
-            if vals.get('relation') and not self.env['ir.model'].search([('model', '=', vals['relation'])]):
-                raise UserError(_("Model %s does not exist!") % vals['relation'])
-            if vals.get('ttype') == 'one2many':
-                if not self.search([('model_id', '=', vals['relation']), ('name', '=', vals['relation_field']), ('ttype', '=', 'many2one')]):
-                    raise UserError(_("Many2one %s on model %s does not exist!") % (vals['relation_field'], vals['relation']))
 
     @api.model
     def create(self, values):
@@ -165,8 +169,6 @@ class CategorizationField(models.Model):
             # If is relational field set relation to js_categorization.value
             if values.get('ttype') in ('many2one', 'many2many'):
                 values.update({'relation': 'js_categorization.value'})
-            # Check restrictions before create
-            self._checkFieldRestrictions(values)
             # Create base field for the model
             self.env['ir.model.fields'].sudo().create(values)
             # Create categorization field
@@ -177,18 +179,16 @@ class CategorizationField(models.Model):
                 # Write fileds to XML
                 self._createFieldsXml()
                 return result
-        except:
+        except Exception, e:
             # Not make changes in db
             self.env.cr.rollback()
-            return False
+            raise UserError(_('Error creating the field \n%s') % (e))
 
     @api.multi
     def write(self, values):
         try:
             # If not updating sequence
             if not (values.get('sequence') and len(values) == 1):
-                # Check restrictions before write
-                self._checkFieldRestrictions(values)
                 # Write model base field
                 self.env['ir.model.fields'].sudo().write(values)
             # Write categorization field
@@ -197,9 +197,10 @@ class CategorizationField(models.Model):
                 self.env.cr.commit()
                 # Write fileds to XML
                 self._createFieldsXml()
-        except:
+        except Exception, e:
             # Not make changes in db
             self.env.cr.rollback()
+            raise UserError(_('Error updating the field \n%s') % (e))
 
     @api.multi
     def unlink(self):
@@ -216,11 +217,14 @@ class CategorizationField(models.Model):
                 self.env.cr.commit()
                 # Write fileds to XML
                 self._createFieldsXml()
-        except:
+        except Exception, e:
             # Not make changes in db
             self.env.cr.rollback()
+            self._createFieldsXml()
+            raise UserError(_('Error deleting the field \n%s') % (e))
 
 class CategorizationValue(models.Model):
     _name = 'js_categorization.value'
-    _description = "Categorization values"
+    _description = "Categorization Values"
+    _sql_constraints = [('categorization_value_unique', 'unique(name)', 'Value must be unique in categorization!')]
     name = fields.Char(required=True, translate=True)
