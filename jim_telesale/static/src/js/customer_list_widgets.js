0b93d7b34535dd060a2a63c3768ade62b6b0ea01
javierjcf <javierjcf@gmail.com>
2019-12-17 18:18:14 +0100

    [ADD]jim_telesale: Persona de contacto

---
+++ jim_telesale/static/src/js/customer_list_widgets.js
@@ -0,0 +1,14 @@
+odoo.define('jim_telesale.customer_list_widgets2', function (require) {
+"use strict";
+var Model = require('web.DataModel');
+var CustomerList = require('telesale.CustomerList');
+var core = require('web.core');
+var _t = core._t;
+
+var CustomerListWidget = CustomerList.CustomerListWidget.include({
+   
+
+});
+
+
+});
