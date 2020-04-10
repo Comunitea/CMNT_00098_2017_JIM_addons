f70d70ce0e160b1cd7a791ebe5ed4dda84af4e2f
Kiko Sánchez <kiko@comunitea.com>
2020-04-10 21:18:02 +0200

    [FIX] custom_documents Si el campo advise estaba vacío no debería de imprimir una nuev apágina

--- custom_documents/models/company.py
+++ custom_documents/models/company.py
@@ -2,7 +2,7 @@
 # © 2017 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResCompany(models.Model):
@@ -16,3 +16,10 @@ class ResCompany(models.Model):
     hide_fields = fields.Boolean()
     iso_purchase_order_text = fields.Html(translate=True)
     company_advise = fields.Html(translate=True)
+    show_company_advise = fields.Boolean("Mostrar texto al final del doc", default=False)
+    class_page = fields.Char(compute="compute_class_page")
+
+    @api.multi
+    def compute_class_page(self):
+        for company_id in self:
+            company_id.class_page = company_id.show_company_advise and 'not-last-page' or ''
