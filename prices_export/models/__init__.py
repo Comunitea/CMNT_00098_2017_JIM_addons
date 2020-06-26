97233171e6696cc40f32355feabb36cbe68a80ba
Pablo <31565658+pablo-lp@users.noreply.github.com>
2020-06-26 17:49:14 +0200

    Quitar prices_export

--- prices_export/models/__init__.py
+++
@@ -1,8 +0,0 @@
-
-# -*- coding: utf-8 -*-
-# © 2020 Comunitea - Javier Colmenero <javier@comunitea.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import export_prices
-from . import pricelist
-from . import product
