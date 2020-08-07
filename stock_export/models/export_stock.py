bd47c777ae4feac45f9bda9bfcfe379912378fe9
Pablo LP <pablo.luaces@gmail.com>
2020-08-07 21:01:00 +0200

    Filtrar por website_published por motivos de rendimiento

--- stock_export/models/export_stock.py
+++ stock_export/models/export_stock.py
@@ -363,7 +363,7 @@ class DeletedObject(models.Model):
         time_now_str = fields.Datetime.to_string(time_now)
         
         if all:
-            domain = [('type', '=', 'product'), '|', ('active', '=', True), ('active', '=', False)]
+            domain = [('type', '=', 'product'), '|', ('active', '=', True), ('active', '=', False), ('website_published', '=', True)]
             product_ids = self.env['product.product'].search(domain)
         else:
             if not from_time:
@@ -426,7 +426,7 @@ class DeletedObject(models.Model):
             print ("-- Evaluando de {} a {}".format(cont-inc, cont)); mid_time = time.time()
             res += [{'variant_id': x[field_id] if x.attribute_names else None,
                      'product_id': x['product_tmpl_id']['id'],
-                     'stock': x[stock_field] if x[stock_field] > 0 else 0} for x in product_ids[cont-inc: cont]]
+                     'stock': x[stock_field] if x[stock_field] > 0 else 0} for x in product_ids[cont-inc: cont] if x['website_published'] == True]
             print ("-- Evaluado. Tiempo: {} ".format(time.time() - mid_time))
             mid_time = time.time()
 
