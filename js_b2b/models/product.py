f0b05d6beb8c93ac45abf02d402b9df7f353422d
Pablo <31565658+pablo-lp@users.noreply.github.com>
2020-08-19 09:53:21 +0200

    Correcciones + Desactivar envío en tiempo real de Prestadoo
    - Error corregido en js_fill_product
    - Envíos realtime desactivados en Prestadoo
    - Cambiado filtro de imágenes para las variantes
    - Configuración de albaranes

--- js_b2b/models/product.py
+++ js_b2b/models/product.py
@@ -167,26 +167,25 @@ class ProductProduct(models.Model):
 
 	@api.one
 	def _filter_variant_images(self):
-		product_attrs = [attr.id for attr in self.attribute_value_ids]
+		product_attrs = self.attribute_value_ids._ids
 
 		self.product_image_ids = self.env['product.image'].search([
 			('product_tmpl_id', '=', self.product_tmpl_id.id)
-		]).filtered(lambda image: all(map(lambda x: x in product_attrs, image.product_attributes_values._ids)))
-		# To exclude images without all attribute of variant use this line instead
-		# filtered(lambda image: all(map(lambda x,y: x in product_attrs and y in image.product_attributes_values._ids, image.product_attributes_values._ids, product_attrs)))
+		# Filter images that contains at least one attribute of variant, not order dependent
+		]).filtered(lambda image: len(tuple(set(product_attrs) & set(image.product_attributes_values._ids))) > 0)
 
 	@api.one
 	@api.depends('product_image_ids')
-	def _compute_variant_images(self):
+	def _compute_variant_cover(self):
 		if self.product_image_ids:
 			self.image_variant = self.product_image_ids[0].image
-		elif self.product_tmpl_id.image_medium:
-			self.image_variant = self.product_tmpl_id.image_medium
+		elif self.product_tmpl_id.image_small:
+			self.image_variant = self.product_tmpl_id.image_small
 		else:
 			self.image_variant = False
 
-	image_variant = fields.Binary(compute='_compute_variant_images', store=False)
 	product_image_ids = fields.One2many('product.image', string='Images', compute='_filter_variant_images')
+	image_variant = fields.Binary(compute='_compute_variant_cover', store=False)
 	website_published = fields.Boolean('Visible on Website', default=False, copy=False)
 
 	@api.multi
