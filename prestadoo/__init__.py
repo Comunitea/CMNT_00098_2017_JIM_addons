a26dd952237c766e9056cdfd986729f3c4ba9086
Pablo <31565658+pablo-lp@users.noreply.github.com>
2020-10-19 13:29:17 +0200

    Eliminar módulos antiguos

--- prestadoo/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2017 Miguel Ángel García <info@miguel-angel-garcia.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-
-import models
