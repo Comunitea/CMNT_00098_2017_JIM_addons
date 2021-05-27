9ead682a8ce738adbbb36a2cf5e175ac38606034
Pablo <31565658+pablo-lp@users.noreply.github.com>
2021-05-27 11:05:19 +0200

    Forzar eliminación de registros creados

--- js_b2b/models/export.py
+++ js_b2b/models/export.py
@@ -316,11 +316,11 @@ class B2BExport(models.Model):
 			resource_name = str(record.name)
 			res_model, res_id = resource_id.split(',')
 			if super(B2BExport, record).unlink():
-				self.env[res_model].browse(int(res_id)).b2b_record('delete', conf_items_before={resource_name: True})
+				self.env[res_model].browse(int(res_id)).with_context(b2b_autolinks=False).b2b_record('delete', conf_items_before={resource_name: True})
 				# Delete related records also
 				for related in self.search([('rel_id', '=', resource_id)]):
 					related_name = str(related.name)
 					rel_model, rel_id = related.res_id.split(',')
 					if related.unlink():
-						self.env[rel_model].browse(int(rel_id)).b2b_record('delete', conf_items_before={related_name: True})
+						self.env[rel_model].browse(int(rel_id)).with_context(b2b_autolinks=False).b2b_record('delete', conf_items_before={related_name: True})
 		return True
