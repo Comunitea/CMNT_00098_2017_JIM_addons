26d7e9289631bb394b0997c9390e94fc9ac2bd56
Omar (Comunitea) <omar@comunitea.com>
2021-08-30 11:15:02 +0200

    [REM] product_harmonized_system_country_tax: Está en los repos de js

--- product_harmonized_system_country_tax/README.md
+++
@@ -1,2 +0,0 @@
-# Product Harmonized System Country Tax
-
--- product_harmonized_system_country_tax/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# © 2016 Comunitea - Kiko Sanchez <kiko@comunitea.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from . import hs_code
-from . import purchase_order
